c685c51932b5b4673fe4a815ab504a9e54afac94
David Tang <dtang859@gmail.com>
2018-04-10 19:35:58 -0700

    in class

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const Genre = require('./models/genre');
 
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+router.get('/api/genres', async (context) => {
+  let genres = await Genre.fetchAll();
+  context.body = genres;
+});
+
+router.get('/api/genres/:id', async (context) => {
+  let id = context.params.id;
+  let genre = new Genre({ GenreId: id });
+  genre = await genre.fetch();
+
+  try {
+    if (!genre) {
+      throw new Error(`Genre ${id} cannot be found`);
+    }
+
+    context.body = genre;
+  } catch (e) {
+    context.status = 404;
+    context.body = {
+      error: e.message
+    };
+  }
+});
+
+router.post('/api/genres', async (context) => {
+  let name = context.request.body.name;
+
+  try {
+    if (!name) {
+      throw new Error('A genre must have a name');
+    }
+
+    let genre = new Genre({ Name: name });
+    let genreWasFound = await genre.fetch();
+
+    if (genreWasFound) {
+      throw new Error(`Genre ${name} already exists`);
+    }
+
+    await genre.save();
+    context.body = genre;
+  } catch (e) {
+    context.status = 422;
+    context.body = {
+      error: e.message
+    };
+  }
+});
+
+app.use(router.routes());
+app.listen(process.env.PORT || 3000);
